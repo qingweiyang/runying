707b09d121deeb15850342d222bd6a41f97e9988
i319213 <kevin.yang05@sap.com>
2015-06-02 17:16:00 +0800

    optimize orders in font design

--- src/main/webapp/font-design/warehouse_check/js/warehouse_cur.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function(){
-    $("#nav-left").load("../plugins/nav_left.html");
-    $("#nav-top").load("../plugins/nav_top.html");
-    $("#footer").load("../plugins/footer.html");
-});
