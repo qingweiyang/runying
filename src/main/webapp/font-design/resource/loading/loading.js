ee2f6611614e6f2cbee66787883b54bc6cd924aa
i319213 <kevin.yang05@sap.com>
2015-06-29 15:14:18 +0800

    change loading ui

--- src/main/webapp/font-design/resource/loading/loading.js
+++ src/main/webapp/font-design/resource/loading/loading.js
@@ -1,7 +1,12 @@
 function loading() {
 	//$('#myModal').modal('show');
-	$("body").append('<div class="modal fade" id="loadingModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"><div class="loadbg col-md-offset-5"><div class="circle1 circle"></div><div class="circle2 circle"></div><div class="circle3 circle"></div></div>');
-
+	if($("body").find("#loadingModal").size() < 1) {
+		$("body").append('<div class="modal fade" id="loadingModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
+				'<div class="spinner">'+
+				'<div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>'+
+				'</div>');
+	}
+	
 	$('#loadingModal').modal({backdrop: 'static', keyboard: false});
 }
 
